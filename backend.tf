8362637e3f482ae37e5194dfe6500c9ff8609e30
nasa7733 <58895722+nasa7733@users.noreply.github.com>
2020-08-19 20:39:17 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,18 +1,7 @@
-######
-#terraform {
-#  backend "gcs" {
-#    bucket      = "storage-tfstate"
-#    credentials = "cred.json"
-#    #credentials = file("cred.json")
-#  }
-#}
-###########
-
 terraform {
   backend "gcs" {
     project = "lyrical-shore-262101"
     bucket  = "terraform-state"
     credentials = "cred.json"
-
   }
 }
