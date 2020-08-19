95d7833a5a7c4752f8d86cef8e5b10db8ac07efb
nasa7733 <58895722+nasa7733@users.noreply.github.com>
2020-08-19 20:32:08 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "gcs" {
     bucket      = "storage-tfstate"
-    credentials = "/.../creds/cred.json"
+    credentials = "cred.json"
     #credentials = file("cred.json")
   }
 }
