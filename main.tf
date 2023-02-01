668bcd4933b3fa40fc669c81a4f6442fa6352d6b
nasa7733 <58895722+nasa7733@users.noreply.github.com>
2023-02-01 23:11:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,3 @@
-# This is the provider used to spin up the gcloud instance
-provider "google" {
- #credentials = file("creds.json")
- project = "gcpkole"
- region  = "us-central1"
- zone     = "us-central1-c"
- 
- 
-}
-
-
 resource "google_compute_instance" "vm-instance" {
   name         = "test01"
   machine_type = "f1-micro"
